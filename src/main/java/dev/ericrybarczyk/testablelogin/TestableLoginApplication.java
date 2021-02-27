05558f6b56d1ee3d14a8819ec5c0d18020263b25
EricRybarczyk <EricRybarczyk@gmail.com>
2021-02-27 10:44:53 -0500

    Initial Commit

---
+++ src/main/java/dev/ericrybarczyk/testablelogin/TestableLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.ericrybarczyk.testablelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestableLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestableLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ericrybarczyk/testablelogin/TestableLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericrybarczyk.testablelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestableLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
